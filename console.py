19229eaf3ba91a3a658afb5eaa16d5f7fa1e3dc7
Martyna Stanislawska <martynastan@gmail.com>
2021-09-29 09:06:35 +0100

    this does not work

---
+++ console.py
@@ -0,0 +1,27 @@
+import pdb
+from models.album import Album
+
+from models.artist import Artist
+import repositories.artist_repository as artist_repository
+import repositories.album_repository as album_repository
+
+artist_repository.delete_all()
+album_repository.delete_all()
+
+
+
+artist1 = Artist("The Beatles")
+artist_repository.save(artist1)
+
+artist2 = Artist("Elvis Presley")
+artist_repository.save(artist2)
+
+album1 = Album("The White Album", "rock", artist1)
+
+
+
+
+
+
+
+pdb.set_trace()
---
+++ repositories/album_repository.py
@@ -0,0 +1,27 @@
+from models.artist import Artist
+from db.run_sql import run_sql
+from models.album import Album
+import repositories.artist_repository as artist_repository
+
+def save(album):
+    sql = f"INSERT INTO albums(title, genre, artist_id) VALUES (%s, %s,%s) RETURNING *"
+    values = [album.title, album.genre, album.artist.id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    album.id = id
+    return album
+
+def select_all():
+    albums = []
+    sql = "SELECT * FROM albums"
+    results = run_sql(sql)
+    for row in results:
+        artist = artist_repository.select(row['artist_id'])
+        album = Album(row['title'], row['genre'], artist, row['id'] )
+        albums.append(album)
+    return albums
+
+def delete_all():
+    sql = "DELETE FROM albums;"
+    run_sql(sql)
+
